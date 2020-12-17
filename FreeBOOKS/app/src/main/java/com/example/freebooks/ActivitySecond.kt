a1d467213d91ae64665225aecb7261ac86e137d4
Ilya <ilya.balashow1w2@gmail.com>
2020-12-17 10:11:41 +0600

    secondCommit

--- FreeBOOKS/app/src/main/java/com/example/freebooks/ActivitySecond.kt
+++ FreeBOOKS/app/src/main/java/com/example/freebooks/ActivitySecond.kt
@@ -1,19 +1,23 @@
 package com.example.freebooks
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.Nullable
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val view
-        var rcView = view.findViewById(R.id.rcView)
+
+        val rcView=  RecyclerView(findViewById(R.id.rcView))
 
         var list = ArrayList<ListItem>()
         list.add(ListItem(R.drawable.book, "Английский язык", "Отдаю в хорошие руки, бесплатно"))
-
-
+        rcView.hasFixedSize()
+        rcView.layoutManager = LinearLayoutManager(this)
         rcView.adapter = MyAdapter(list, this)
 
     }
--- FreeBOOKS/app/src/main/java/com/example/freebooks/MainActivity.kt
+++ FreeBOOKS/app/src/main/java/com/example/freebooks/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.example.freebooks
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import com.example.freebooks.R.id.rcView
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
-        rcView = view.findViewById(rcView)
+        setContentView(R.layout.activity_main)
 
-
-        var list = ArrayList<ListItem>()
-        list.add(ListItem(R.drawable.book, "Английский язык", "Отдаю в хорошие руки, бесплатно"))
-
-        rcView.adapter = MyAdapter(list, this)
     }
+
 }
+
